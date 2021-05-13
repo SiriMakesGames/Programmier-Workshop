2596bd84211a5eb6b172499c462aa19f3e457052
Siri <sirimakesgames@gmail.com>
2021-05-13 15:51:06 +0200

    namespace for step 11

--- Assets/Assets by Steps/Step 11 - Count Bounces/BounceCounter.cs
+++ Assets/Assets by Steps/Step 11 - Count Bounces/BounceCounter.cs
@@ -1,37 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 
-public class BounceCounter : MonoBehaviour
+namespace Step11
 {
-    [SerializeField] private TextMeshProUGUI counterLabel;
-    private int counter;
-    
-    private void Awake()
+    public class BounceCounter : MonoBehaviour
     {
-        Reset();
-    }
-
-    private void UpdateBounceLabel()
-    {
-        counterLabel.text = counter.ToString();
-    }
-
-    private void OnTriggerExit2D(Collider2D collider2D)
-    {
-        if(collider2D.gameObject.tag != "Ball")
+        [SerializeField] private TextMeshProUGUI counterLabel;
+        private int counter;
+        
+        private void Awake()
         {
-            return;
+            Reset();
         }
 
-        counter++;
-        UpdateBounceLabel();
-    }
+        private void UpdateBounceLabel()
+        {
+            counterLabel.text = counter.ToString();
+        }
 
-    public void Reset()
-    {
-        counter = 0;
-        UpdateBounceLabel();
+        private void OnTriggerExit2D(Collider2D collider2D)
+        {
+            if(collider2D.gameObject.tag != "Ball")
+            {
+                return;
+            }
+
+            counter++;
+            UpdateBounceLabel();
+        }
+
+        public void Reset()
+        {
+            counter = 0;
+            UpdateBounceLabel();
+        }
     }
 }
+
