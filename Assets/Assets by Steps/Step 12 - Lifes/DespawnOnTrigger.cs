e932d4447e41a806cb75520bde64b4c1be64add2
Siri <sirimakesgames@gmail.com>
2021-05-21 16:04:07 +0200

    Fix step 12 despawnOnTrigger tag

--- Assets/Assets by Steps/Step 12 - Lifes/DespawnOnTrigger.cs
+++ Assets/Assets by Steps/Step 12 - Lifes/DespawnOnTrigger.cs
@@ -7,7 +7,7 @@ public class DespawnOnTrigger : MonoBehaviour
     [SerializeField] LifeCounter lifeCounter;
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
-        if(collider2D.gameObject.layer != 8)
+        if(collider2D.gameObject.tag != "Ball")
         {
             return;
         }
